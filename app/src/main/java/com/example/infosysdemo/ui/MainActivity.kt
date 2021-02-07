236f0ab8ea377f1a0aa2be24105f249382ede0d8
harsha <harshab.ss@smartdatainc.net>
2021-02-07 16:12:06 +0530

    setup dependency library.

---
+++ app/src/main/java/com/example/infosysdemo/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.infosysdemo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.infosysdemo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
